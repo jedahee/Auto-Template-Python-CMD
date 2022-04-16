2694c1e5ea7112dbe23b89f103d31875b6df075d
Jesus <jedahee02@gmail.com>
2022-04-16 21:02:58 +0200

    Fixing bugs

--- function.py
+++ function.py
@@ -6,22 +6,39 @@
 #              with technologies like Bootstrap 5, Tailwind, 
 #              HTML / CSS / JS, JQuery (Normal version, Mobile version, UI version)
 
-class Function():
-    def __init__(self, directory_name, file_name):
-        self.directory_name = directory_name
-        self.file_name = file_name
+# IMPORT
+# ------
 
-        self.options = {1: "Select project", 
-                        2: "Set folder name and file name",
-                        3: "Create template", 
-                        0: "Exit"}
+# External
+# --------
+import sys
+import os
+import platform
+import webbrowser
+
+class Function():
+    def __init__(self, folder_name, file_name, path):
+        self.folder_name = "new_folder"
+        self.file_name = "new_file"
+        
+        if platform.system() == "Linux":
+            self.path = "/home"
+        elif platform.system() == "Windows":
+            self.path = "C:/"
+
+
+        self.options = {
+            1: "Select project", 
+            2: "Set folder name and file name",
+            3: "Create template", 
+            0: "Exit"
+        }
+        
         self.projects = {
             "Bootstrap": {
-                "(last) version_0": 5,
+                "(last) version_0": "5.1.3",
             },
-            "Tailwind": {
-                "(last) version_0": 3,
-            },
+            "Tailwind": {},
             "HTML": {
                 "preprocessor_0": "CSS",
                 "preprocessor_1": "SCSS",
@@ -35,13 +52,245 @@ class Function():
 
         }
 
+    def create_folder(self):
+        try:
+            os.mkdir(self.path + "/" + self.folder_name)
+        except (PermissionError ):
+            print("\n ERROR: Permiso dengado, no se puede crear el proyecto en esta ruta " + self.path + " \n") 
+
+    def create_template(self, project, version):
+        self.create_folder()
+        
+        if project == "Bootstrap":
+            self.create_html_bootstrap(version)
+            self.create_css()
+            self.create_js()
+        elif project == "Tailwind":
+            self.create_html_tailwind()
+            self.create_css()
+            self.create_js()
+        elif project == "HTML":
+            self.create_html()
+            
+            if version == "CSS":
+                self.create_css()
+            elif version == "SCSS":
+                self.create_scss()
+            elif version == "Less":
+                self.create_less()
+            
+            self.create_js()
+        
+        elif project == "JQuery":
+            self.create_html_jquery(version)
+
+    def create_html(self):
+        content = ''' 
+            <!DOCTYPE html>
+            <html lang="es">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link rel="stylesheet" href="''' + self.file_name + '''">
+                <title>Document</title>
+            </head>
+            <body>
+                <!-- Auto-generated with autoTemplate -->        
+                <script src="''' + self.file_name + '''"></script>
+            </body>
+            </html>
+        '''
+        
+        file = open(self.path + "/" + self.folder_name + "/" + self.file_name + ".html")
+        file.write(contet)
+        file.close()
+
+    def create_html_bootstrap(self, version):
+        content = ''' 
+            <!DOCTYPE html>
+            <html lang="es">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link rel="stylesheet" href="''' + self.file_name + '''">
+                <!-- CSS only -->
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@'''+version+'''/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+                <title>Document</title>
+            </head>
+            <body>
+                <!-- Auto-generated with autoTemplate -->
+                <p class="display-1">Auto-generated with autoTemplate</p>        
+                <script src="''' + self.file_name + '''"></script>
+            </body>
+            </html>
+        '''
+        file = open(self.path + "/" + self.folder_name + "/" + self.file_name + ".html")
+        file.write(contet)
+        file.close()
+
+    def create_html_jquery(self, version):
+        link_jquery_normal = '<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>'
+        link_jquery_ui = '<script src="https://code.jquery.com/ui/1.13.1/jquery-ui.min.js" integrity="sha256-eTyxS0rkjpLEo16uXTS0uVCS4815lc40K2iVpWDvdSY=" crossorigin="anonymous"></script>'
+        link_jquery_mobile = '<script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js" integrity="sha256-Lsk+CDPOzTapLoAzWW0G/WeQeViS3FMzywpzPZV8SXk=" crossorigin="anonymous"></script>'
+        
+        if (version == "Normal"):
+            link_selected = link_jquery_normal
+        elif (version == "Mobile"):
+            link_selected = link_jquery_mobile
+        elif (version == "UI"):
+            link_selected = link_jquery_ui
+
+        content = ''' 
+            <!DOCTYPE html>
+            <html lang="es">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link rel="stylesheet" href="''' + self.file_name + '''">
+                <title>Document</title>
+            </head>
+            <body>
+                <!-- Auto-generated with autoTemplate -->        
+                <script src="''' + self.file_name + '''"></script>
+
+                <!-- JQuery Link -->
+
+                ''' + link_selected + '''
+                
+            </body>
+            </html>
+        '''
+        file = open(self.path + "/" + self.folder_name + "/" + self.file_name + ".html")
+        file.write(contet)
+        file.close()
+
+    def create_html_tailwind(self):
+        content = ''' 
+            <!DOCTYPE html>
+            <html lang="es">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link rel="stylesheet" href="''' + self.file_name + '''">
+                <script src="https://cdn.tailwindcss.com"></script>
+
+                <title>Document</title>
+            </head>
+            <body>
+                <!-- Auto-generated with autoTemplate -->
+                <h1 class="text-3xl font-bold underline">
+                    Auto-generated with autoTemplate
+                </h1>        
+                <script src="''' + self.file_name + '''"></script>
+            </body>
+            </html>
+        '''
+        file = open(self.path + "/" + self.folder_name + "/" + self.file_name + ".html")
+        file.write(contet)
+        file.close()
+
+    def create_css(self):
+        content = ''' 
+            /* Auto-generated with autoTemplate */
+
+            /* RESET */
+            *, button, input, textarea, select {
+                box-sizing: border-box;
+                outline: none;
+                border: none;
+                list-style: none;
+                text-decoration: none;
+                text-emphasis: none;
+            }
+        '''
+        file = open(self.path + "/" + self.folder_name + "/" + self.file_name + ".css")
+        file.write(contet)
+        file.close()
+
+    def create_js(self):
+        content = ''' 
+            // Auto-generated with autoTemplate
+
+            window.onload = function(e) {
+                console.log('Auto-generated with autoTemplate')
+            }
+        '''
+        file = open(self.path + "/" + self.folder_name + "/" + self.file_name + ".js")
+        file.write(contet)
+        file.close()
+
+    def create_scss(self):
+        content = ''' 
+            /* Auto-generated with autoTemplate */
+
+            /* RESET */
+            *, button, input, textarea, select {
+                box-sizing: border-box;
+                outline: none;
+                border: none;
+                list-style: none;
+                text-decoration: none;
+                text-emphasis: none;
+            }
+        '''
+        file = open(self.path + "/" + self.folder_name + "/" + self.file_name + ".scss")
+        file.write(contet)
+        file.close()
+
+    def create_less(self):
+        content = ''' 
+            /* Auto-generated with autoTemplate */
+
+            /* RESET */
+            *, button, input, textarea, select {
+                box-sizing: border-box;
+                outline: none;
+                border: none;
+                list-style: none;
+                text-decoration: none;
+                text-emphasis: none;
+            }
+        '''
+        file = open(self.path + "/" + self.folder_name + "/" + self.file_name + ".less")
+        file.write(contet)
+        file.close()
+
+    def set_path(self, path):
+        self.path = path if os.path.exists(path) else ""
+        return self.path
+
+    def set_fileName(self, file_name):
+        self.file_name = file_name if file_name != "" else self.file_name
+
+    def set_folderName(self, folder_name):
+        self.folder_name = folder_name if folder_name != "" else self.folder_name
+
+    def get_info(self, project, details_project):
+        print("Path: " + str(self.path), end="\t")
+        print("Folder name: " + str(self.folder_name))
+        print("File name: " + str(self.file_name), end="\t")
+        print("Project:  " + project + " (" + details_project + ") ")
+
+    def print_keys_from_projects(self, project):
+        i = 0
+
+        for value in self.projects[project].values():
+            print("["+ list(self.projects[project].keys())[i] +"] -> " + str(value))
+            i += 1
+
+    def in_array(self, option, array):
+        return option in array;
+
     def print_options(self):
-        print("\n<-- OPTIONS (Select a numeric index) -->\n")
         for key, value in self.options.items():
             print("-> "+str(key)+" <- " + value)
         
     def print_projects(self):
-        print("\n <-- TEMPLATES --> \n")
         for key, value in self.projects.items():
             print("--> "+key)
 
--- script.py
+++ script.py
@@ -13,7 +13,8 @@
 import function as fc
 
 # External
-import sys, os
+# --------
+
 
 # VARIABLES
 # ---------
@@ -21,10 +22,13 @@ import sys, os
 # Strings
 folder_name = ""
 file_name = ""
-project_option = ""
+path = ""
+project_selected = "HTML"
+details_project_selected = "CSS"
 
 # Integers
 opc = -1
+i = 0
 
 # Floats
 
@@ -34,18 +38,24 @@ opc = -1
 
 
 # Initializing objects
-func = fc.Function(folder_name, file_name)
+func = fc.Function(folder_name, file_name, path)
 
 func.print_menu()
 
 # LOOP
 # ----
 while (opc != 0):
+    #Printing info
+    print("\n<-- INFO -->\n")
+    func.get_info(project_selected, details_project_selected)
+
     opc = -1
 
+    # Validating option
     try:
         
         while (opc < 0 or opc > len(func.options) - 1):
+            print("\n<-- OPTIONS (Select a numeric index) -->\n")
             func.print_options()
 
             opc = input(">> ")
@@ -56,41 +66,43 @@ while (opc != 0):
     except:
         print("\n ERROR: You must select a numeric index option\n")
 
+    # Log out
     if (opc == 0):
         print("\n### Bye! :) ###") 
+    
+    # Validating options from project selected
     elif (opc == 1):
+        print("\n <-- TEMPLATES --> \n")
         func.print_projects()
-        project_option = input(">> ")
 
-        if (project_option in func.projects.keys()):
-            print("\n")
+        project_selected = input(">> ")
 
-            for value in func.projects[project_option].values():
-                print("[preprocessor] -> " + str(value))
+        while (project_selected not in func.projects.keys()):
             
-            print("\n")
-        
-        else:
             print("\nERROR: You must select a right option\n")
+            project_selected = input(">> ")
 
-            while (project_option not in func.projects.keys()):
-                project_option = input(">> ")
+        else:
+            if (len(func.projects[project_selected]) > 0):
+                print("\n")
+                func.print_keys_from_projects(project_selected)
 
-                if (project_option in func.projects.keys()):
-                    print("\n")
-            
-                    for value in func.projects[project_option].values():
-                        print("[preprocessor] -> " + str(value))
-                    
-                    print("\n")
-                else:
-                    print("\nERROR: You must select a right option\n")
-
+                details_project_selected = input(" >> ")
+                
+                while not func.in_array(details_project_selected, func.projects[project_selected].values()):
+                    print("\n ERROR: You must select a right option \n")
+                    details_project_selected = input(" >> ")
 
     elif (opc == 2):
-        pass
-    elif (opc == 3):
-        pass
+        path = input("(/home/user/) Path >> ")       
         
-        
+        while func.set_path(path) == "":
+            print("\n ERROR: Path does not exist \n")
+            path = input("(/home/user/) Path >> ") 
 
+        folder_name = func.set_folderName(input("Folder name >> "))       
+        file_name = func.set_fileName(input("File name >> "))
+    
+    elif (opc == 3):
+        func.create_template(project_selected, details_project_selected)
+        
